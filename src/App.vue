26df242e37f51b7bd66a1ee8aa159d79efda9c14
samtumo <samvel.k40@tumo.org>
2025-07-17 19:32:51 +0400

    niga

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref} from 'vue';
+import Card from './components/Card.vue'
+let number = ref (0)
+const increment = () => {number.value++
+
+}
+ </script>
+
+<template>
+<Card :number="number"/>
+  <button @click = "increment">click me </button>
+  
+</template>
+
+<style scoped>
+
+h1 {color:red;}
+h2 {color:blue;}
+h1,h2{background-color: yellow;}
+
+</style>
+  
---
+++ src/components/Card.vue
@@ -0,0 +1,10 @@
+<template><h1>{{ props.number }}</h1>
+  <h2>niga</h2></template>
+
+
+<script setup> 
+import {defineProps} from 'vue';
+let props = defineProps(['number'])
+ </script>
+
+<style></style>
