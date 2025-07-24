ade5a54a41c36dca7fbcfe8913cba1adc29d226c
samtumo <samvel.k40@tumo.org>
2025-07-24 19:05:03 +0400

    das 6

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
 import {ref} from 'vue';
 import AddButton from './components/AddButton.vue'
 import Card from './components/Card.vue';
+import NoteForm from './components/NoteForm.vue';
+
+let isOpen = ref(false);
+
+function toggleOpen() {
+if (isOpen.value === true) {
+    isOpen.value = false
+    
+}else {
+  isOpen.value = true
+}
+}
 
 
  </script>
@@ -9,9 +21,10 @@ import Card from './components/Card.vue';
 <template>
 <div class="main">
   <h1 class="title">Note App</h1>
-<AddButton/>
+<AddButton @click="toggleOpen"/>
+<NoteForm v-if="isOpen" @onClose="toggleOpen"/>
 <div class="notes">
-  <Card/>
+  <Card title="My Text"/>
   <Card/>
   <Card/>
 
@@ -26,9 +39,11 @@ import Card from './components/Card.vue';
   background: linear-gradient(128deg, rgba(0, 191, 255, 0.84) 12.75%, rgba(255, 255, 255, 0.67) 83.4%);
   font-size: 40px;
   font-weight: 500;
+ 
 }
 .title{
 text-align: center;
+padding: 20px;
 }
 .notes{
   display: flex;
